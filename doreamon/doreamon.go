e289768ec71f4a3a5b4239eb1e8daa99073165b2
whatwewant <tobewhatwewant@outlook.com>
2023-02-04 23:52:39 +0800

    feat(doreamon): support user api => GET /api/user

--- doreamon/doreamon.go
+++ doreamon/doreamon.go
@@ -97,7 +97,11 @@ func Serve(cfg *Config) error {
 			}
 
 			client.Callback(code, state, func(user *oauth2.User, token *oauth2.Token, err error) {
-				ctx.Session().Set("oauth2.user", user.ID)
+				userSessionKey := fmt.Sprintf("user:%s", user.ID)
+
+				ctx.Cache().Set(userSessionKey, user, ctx.App.SessionMaxAge)
+
+				ctx.Session().Set("oauth2.user", userSessionKey)
 				// ctx.Session().Set("oauth2.token", token.AccessToken)
 
 				ctx.Redirect(originFrom)
@@ -114,12 +118,45 @@ func Serve(cfg *Config) error {
 			return
 		}
 
+		if ctx.Path == "/api/user" {
+			userSessionKey := ctx.Session().Get("oauth2.user")
+			if userSessionKey == "" {
+				if ctx.AcceptJSON() {
+					ctx.JSON(http.StatusUnauthorized, zoox.H{
+						"code":    401001,
+						"message": "unauthorized",
+					})
+					return
+				}
+
+				ctx.Redirect(fmt.Sprintf("/login?from=%s&reason=%s", url.QueryEscape(ctx.Path), "user not login or token expired"))
+				return
+			}
+			user := oauth2.User{}
+			if err := ctx.Cache().Get(userSessionKey, &user); err != nil {
+				time.Sleep(1 * time.Second)
+				ctx.Redirect(fmt.Sprintf("/login?from=%s&reason=%s", url.QueryEscape(ctx.Path), "user cache not found"))
+				return
+			}
+
+			ctx.JSON(http.StatusOK, user)
+			return
+		}
+
 		if ctx.Session().Get("oauth2.user") == "" {
 			originFrom := ctx.Request.RequestURI
 			time.Sleep(1 * time.Second)
 			ctx.Redirect(fmt.Sprintf("/login?from=%s", url.QueryEscape(originFrom)))
 			return
 		}
+
+		// userSessionKey := ctx.Session().Get("oauth2.user")
+		// user := oauth2.User{}
+		// if err := ctx.Cache().Get(userSessionKey, &user); err != nil {
+		// 	time.Sleep(1 * time.Second)
+		// 	ctx.Redirect(fmt.Sprintf("/login?reason=%s", "user cache not found"))
+		// 	return
+		// }
 
 		ctx.Next()
 	})
